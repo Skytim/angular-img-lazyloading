b9be065410b40ed0b5132b3671b60ba1cffc778f
Skytim <sky22357168@gmail.com>
2020-08-31 14:32:48 +0800

    Angular img lazing loading directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  public values: number[] = [];
+  constructor() {
+    for (let index = 400; index < 500; index++) {
+      this.values.push(index);
+    }
+  }
 }
